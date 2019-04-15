99f278db17c2af5fa4590ba1a881854cd290edc2
Believe Ohiozua <38279402+believeohiozua@users.noreply.github.com>
2019-04-15 15:39:56 +0100

    Update summary.py

--- summary.py
+++ summary.py
@@ -5,6 +5,8 @@ import queue
 import time
 from newspaper import Article
 import nltk
+import heapq
+import re
 
 
 #define input variable
@@ -39,26 +41,73 @@ def main():
                 article.download()
                 article.parse() 
                 article.nlp()
+                
+                # Removing Square Brackets and Extra Spaces
+                article_text = re.sub(r'\[[0-9]*\]', ' ', article.text)  
+                article_text = re.sub(r'\s+', ' ', article_text) 
+                
+                # Removing special characters and digits from the text
+                formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text)  
+                formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+                
+                #sentence tokenization
+                sentence_list = nltk.sent_tokenize(article.text)
+                
+                #Weighted Frequency of Occurrence with ~formatted_article_text~
+                stopwords = nltk.corpus.stopwords.words('english')
+                word_frequencies = {}  
+                for word in nltk.word_tokenize(formatted_article_text):  
+                    if word not in stopwords:
+                        if word not in word_frequencies.keys():
+                            word_frequencies[word] = 1
+                        else:
+                            word_frequencies[word] += 1 
+                            
+                 #maximum frequncy of Occurrence           
+                maximum_frequncy = max(word_frequencies.values())
+                for word in word_frequencies.keys():  
+                    word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+
+                
+                 #Calculating Sentence Scores           
+                sentence_scores = {}  
+                for sent in sentence_list:  
+                    for word in nltk.word_tokenize(sent.lower()):
+                        if word in word_frequencies.keys():
+                            if len(sent.split(' ')) < 30:
+                                if sent not in sentence_scores.keys():
+                                    sentence_scores[sent] = word_frequencies[word]
+                                else:
+                                    sentence_scores[sent] += word_frequencies[word]
+                                    
+                 #Getting the Summary 
+                summary_sentences = heapq.nlargest(10, sentence_scores, key=sentence_scores.get)
+                summary = ' '.join(summary_sentences)  
+                
+                
                 #print keywords
                 print('\n keywords; \n',article.keywords)
                 #print page summary
-                print('\n summary :\n',article.summary)
+                print('\n summary :\n',summary)
             except:
                 print('\n\n Invalid Entry!, please Ensure you enter a valid web link \n\n')
+                
+                
+             #close the loop
+            if (input_str == EXIT_COMMAND):
+                print("Oops! You Have exited the program")
+                break   
                 
             #restart again
             input_str = ' '
             print("\n\n Type/Paste a valid hotel web link below or 'exit\' to close \n  {}".format(input_str))
             
-			#close the loop
-            if (input_str == EXIT_COMMAND):
-                print("Exiting serial terminal.")
-                break           
+			        
 
         
         #auto end section
         time.sleep(0.1) 
-    print("End.")
+
 
 if (__name__ == '__main__'): 
     main()
