031a946a5a6022aebf93750f8f19a875c12d288f
Akinduko Olugbenga <oakinduko@MACs-MacBook-Pro-3.local>
2019-04-15 15:51:10 +0100

    feat: local commits

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 # load Dependancies
 import sys
 import os
-import threading
-import queue
 import time
-import nltk
 import textwrap
 import logging
 import signal
 import argparse
-from newspaper import Article
+import numpy as np
+import pandas as pd
+import nltk
+nltk.download('punkt') # one time execution
+import re
 
 
 def parse_args(argv):
@@ -53,11 +54,22 @@ def main(argv=sys.argv):
             # # print keywords
             # print('\n keywords; \n', article.keywords)
             # print page summary
-            print('\n summary :\n\n', article.summary,'\n\n')
+            print('\n summary :\n\n', article.summary, '\n\n')
             sys.stdout.flush()
         except:
             print('\n\n Invalid Entry!, please Ensure you enter a valid web link \n\n')
             sys.stdout.flush()
+    if action == 'train':
+        # default path for the folder inside google drive
+        default_path = "drive/Colab Notebooks/Model 2/"
+        # path for training text (article)
+        train_article_path = default_path + "sumdata/train/train.article.txt"
+        # path for training text output (headline)
+        train_title_path = default_path + "sumdata/train/train.title.txt"
+        # path for validation text (article)
+        valid_article_path = default_path + "sumdata/train/valid.article.filter.txt"
+        # path for validation text output(headline)
+        valid_title_path = default_path + "sumdata/train/valid.title.filter.txt"
 
 
 if (__name__ == '__main__'):
